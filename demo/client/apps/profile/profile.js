e77ee57b6d15f90045f8951991431268bd9aca63
Dave Geddes <davidcgeddes@gmail.com>
2014-08-01 09:27:41 -0600

    Update 2014-08-01T15:27:41.583Z

--- demo/client/apps/profile/profile.js
+++
@@ -1,8 +0,0 @@
-angular.module('profile', [])
-  .config(angular.module('overmind').control())
-  .config(function($routeProvider){
-    $routeProvider.when('/profile', {controller: 'ProfileCtrl', templateUrl: '/client/apps/profile/profile.html'})
-  })
-  .controller('ProfileCtrl', function($scope){
-    console.log("profile ctrl");
-  })
